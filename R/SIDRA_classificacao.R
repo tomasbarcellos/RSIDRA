0b76aafb4f655bd9bde231c48fb4f8aea9ae4f22
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-01-19 17:37:06 -0200

    Add files via upload

--- R/SIDRA_classificacao.R
+++ R/SIDRA_classificacao.R
@@ -5,12 +5,12 @@
 #' @keywords IBGE SIDRA dados
 #' @export
 #' @examples
-#' cat_PAM <- SIDRA_cat(1612)
+#' cat_PAM <- SIDRA_classificacao(1612)
 
 SIDRA_classificacao <- function(tabela) {
   pag <- read_html(paste0("http://api.sidra.ibge.gov.br/desctabapi.aspx?c=",
                           tabela))
-  
+
   y <- pag %>% html_nodes("table") %>% html_text() %>%
     stringr::str_split("\r\n") %>% lapply(tm::stripWhitespace) %>%
     lapply(function(x) x[!x %in% c(" ", "")]) %>%
@@ -19,13 +19,13 @@ SIDRA_classificacao <- function(tabela) {
                                          2,nchar(x[substr(x, 1,1) == " "]))
       x
     })
-  
+
   nomes_cat <- grep(pattern = "/.*?/", x = y[[3]], value = TRUE) %>%
     gsub(pattern = "/", replacement = "")
   y[[3]] <- NULL
-  
+
   cat <- vector("list", length(y) - 3)
-  
+
   for (i in seq_along(cat)) {
     cat[[i]] <- y[[2+i]] %>% (function(x) {
       x[substr(x, 1,1) %in% 0:9]
@@ -38,13 +38,13 @@ SIDRA_classificacao <- function(tabela) {
       as.data.frame(stringsAsFactors = FALSE) %>%
       `names<-`(c("categoria", "descrição"))
   }
-  
+
   tamanho_cat <- sapply(cat, nrow)
   classificacao <- rep(nomes_cat, times = tamanho_cat)
-  
+
   df_cat <- cbind(classificacao,
                do.call(rbind, cat),
                stringsAsFactors = FALSE)
-  
+
   return(df_cat)
 }
