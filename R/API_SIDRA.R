44490028c2551b53ba27501538e65538dc9ffd49
Tomás Barcellos <tomasbarcellos@gmail.com>
2017-01-23 16:02:02 -0200

    Update API_SIDRA.R
    Este envio tenta contornar o limite de 50 mil registros por solicitação estabelecido pela API da SIDRA.

--- R/API_SIDRA.R
+++ R/API_SIDRA.R
@@ -14,55 +14,70 @@
 #' @examples
 #' PAM <- API_SIDRA(1612, 81)
 
-API_SIDRA <- function(tabela,
-                      classificador = SIDRA_classificacao(tabela), cod_cat = rep("all", length(classificador)),
-                      nivel = 1, cod_nivel = rep("all", length(nivel)),
-                      periodo = "all", variavel = "allxp",
-                      inicio, fim) {
-  # Início das verificações
+API_SIDRA <- function (tabela, classificador = SIDRA_classificacao(tabela), 
+          cod_cat = rep("all", length(classificador)), nivel = 1,
+          cod_nivel = rep("all", length(nivel)),
+          periodo = "all", variavel = "allxp", 
+          inicio, fim) 
+{
   if (length(tabela) > 1) {
-    stop("Solicite os dados de uma tabela por vez. Para mais de uma use mapply()", call. = FALSE)
-    }
-
+    stop("Solicite os dados de uma tabela por vez. Para mais de uma use mapply()", 
+         call. = FALSE)
+  }
   if (!tabela %in% RSIDRA::tabelas_SIDRA$tabela) {
     stop("A tabela informada não é válida", call. = FALSE)
   }
-
   if (!missing(inicio) && !missing(fim)) {
     periodo <- paste0(inicio, "-", fim)
   }
-
   if (length(nivel) != length(cod_nivel)) {
-    stop("Os argumentos nivel e cod_nivel devem ter o mesmo tamanho", call. = FALSE)
+    stop("Os argumentos nivel e cod_nivel devem ter o mesmo tamanho", 
+         call. = FALSE)
   }
-
   cod_nivel <- lapply(cod_nivel, paste, collapse = ",")
-
   area <- paste0("/n", nivel, "/", cod_nivel, collapse = "")
-
   if (length(classificador) != length(cod_cat)) {
-    stop("Os argumentos 'classificador' e 'cod_cat' devem ter o mesmo tamanho", call. = FALSE)
+    stop("Os argumentos 'classificador' e 'cod_cat' devem ter o mesmo tamanho", 
+         call. = FALSE)
   }
-
   cod_cat <- lapply(cod_cat, paste, collapse = ",")
-
   cod_cat <- gsub(pattern = "\\+", replacement = "%20", x = cod_cat)
-
   if (length(classificador) == 0) {
     categ <- classificador
-  } else {
+  }
+  else {
     categ <- paste0("/c", classificador, "/", cod_cat, collapse = "")
   }
-
   url_fixa <- "http://api.sidra.ibge.gov.br/values"
-
-  url_variavel <- paste0("/t/", tabela, "/p/", periodo,
-                         "/v/", variavel, area, categ)
-
+  url_variavel <- paste0("/t/", tabela, "/p/", periodo, "/v/", 
+                         variavel, area, categ)
   resp <- httr::GET(paste0(url_fixa, url_variavel))
-
-  res <- jsonlite::fromJSON(httr::content(resp, "text",
-                                          encoding = "UTF-8"),
+  
+  # verificação do conteúdo
+  
+  if (grepl("excedeu o limite", x = conteudo) == TRUE) {
+    message(paste(
+      conteudo,
+      "Vamos contornar este problema fazendo varias solicitações menores.",
+      "Haverá maior demora", sep = "\n"))
+    valores_solicitados <- stringr::str_extract(conteudo, "[0-9]+") %>% as.numeric()
+    
+    periodos <- SIDRA_periodo(tabela)
+    requisicoes <- (valores_solicitados %/% 50000) + 1
+    
+    cada <- periodos %>% split(cut(seq_along(periodos), requisicoes)) %>%
+      lapply(range) %>% sapply(paste0, collapse = "-")
+    
+    lista_resposta <- lapply(cada, API_SIDRA,
+                             tabela = tabela, classificador = classificador,
+                             cod_cat = cod_cat, nivel = nivel,
+                             cod_nivel = cod_nivel, variavel = variavel)
+    return(do.call("rbind", lista_resposta) %>% as.data.frame())
+  }
+  
+  # Conteúdo já derificado
+  
+  res <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"), 
                             simplifyVector = FALSE)
   res <- do.call("rbind", res)
   res <- as.data.frame(res)
@@ -70,9 +85,9 @@ API_SIDRA <- function(tabela,
   res <- as.data.frame(res, stringsAsFactors = FALSE)
   names(res) <- res[1, ]
   res <- res[-1, ]
-  numericas <- c(grep(pattern = "\\(Código\\)", names(res)), length(res))
-  res[, numericas] <- suppressWarnings(sapply(res[, numericas], as.numeric))
-
+  numericas <- c(grep(pattern = "\\(Código\\)", names(res)), 
+                 length(res))
+  res[, numericas] <- suppressWarnings(sapply(res[, numericas], 
+                                              as.numeric))
   return(res)
 }
-
